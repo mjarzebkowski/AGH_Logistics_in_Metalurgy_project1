8cf90749cb25d935463cd3cffd5ea872d38b11ef
Maciej <maciej.jarzebkowski@gmail.com>
2017-12-10 14:41:56 +0100

    Create CMP class content

--- AGH_Logistyka_p1/CPM_Element.cs
+++ AGH_Logistyka_p1/CPM_Element.cs
@@ -8,6 +8,23 @@ namespace AGH_Logistyka_p1
 {
     public class CPM_Element
     {
+        public int Id { get; set; }
+        public string Name { get; set; } //nazwa 
+
+        public LinkedList<CPM_Element> PreviousElement { get; set; }
+
+        public int tc { get; set; }
+
+        public int tm { get; set; }
+
+        public int tp { get; set; }
+
+        public int t0 { get; set; }
+
+
+
+
+
 
     }
 }
