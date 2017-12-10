71a9647badf005418a357fbf567c735784bc2233
Maciej <maciej.jarzebkowski@gmail.com>
2017-12-10 14:44:42 +0100

    Add CPM_Element class as datasource to Data grids Vieaws

--- AGH_Logistyka_p1/Form1.Designer.cs
+++ AGH_Logistyka_p1/Form1.Designer.cs
@@ -28,26 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.cPMElementBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.previousElementDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tcDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tmDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tpDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.t0DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cPMElementBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.idDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.previousElementDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tcDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tmDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tpDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.t0DataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cPMElementBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cPMElementBindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -82,6 +101,34 @@
             this.groupBox1.Text = "Lista zadań w projekcie";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.previousElementDataGridViewTextBoxColumn,
+            this.tcDataGridViewTextBoxColumn,
+            this.tmDataGridViewTextBoxColumn,
+            this.tpDataGridViewTextBoxColumn,
+            this.t0DataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.cPMElementBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(214, 216);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 241);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Otwórz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(6, 241);
@@ -102,6 +149,24 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Ściezka krytyczna";
             // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoGenerateColumns = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn1,
+            this.nameDataGridViewTextBoxColumn1,
+            this.previousElementDataGridViewTextBoxColumn1,
+            this.tcDataGridViewTextBoxColumn1,
+            this.tmDataGridViewTextBoxColumn1,
+            this.tpDataGridViewTextBoxColumn1,
+            this.t0DataGridViewTextBoxColumn1});
+            this.dataGridView2.DataSource = this.cPMElementBindingSource1;
+            this.dataGridView2.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(214, 173);
+            this.dataGridView2.TabIndex = 0;
+            // 
             // chart1
             // 
             chartArea1.Name = "ChartArea1";
@@ -118,16 +183,6 @@
             this.chart1.TabIndex = 7;
             this.chart1.Text = "chart1";
             this.chart1.Click += new System.EventHandler(this.chart1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(112, 241);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(108, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Otwórz";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -149,21 +204,97 @@
             this.openFileDialog1.FileName = "openFileDialog1";
             this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
-            // dataGridView1
+            // cPMElementBindingSource
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(214, 216);
-            this.dataGridView1.TabIndex = 7;
+            this.cPMElementBindingSource.DataSource = typeof(AGH_Logistyka_p1.CPM_Element);
             // 
-            // dataGridView2
+            // idDataGridViewTextBoxColumn
             // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(6, 19);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(214, 173);
-            this.dataGridView2.TabIndex = 0;
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // previousElementDataGridViewTextBoxColumn
+            // 
+            this.previousElementDataGridViewTextBoxColumn.DataPropertyName = "PreviousElement";
+            this.previousElementDataGridViewTextBoxColumn.HeaderText = "PreviousElement";
+            this.previousElementDataGridViewTextBoxColumn.Name = "previousElementDataGridViewTextBoxColumn";
+            // 
+            // tcDataGridViewTextBoxColumn
+            // 
+            this.tcDataGridViewTextBoxColumn.DataPropertyName = "tc";
+            this.tcDataGridViewTextBoxColumn.HeaderText = "tc";
+            this.tcDataGridViewTextBoxColumn.Name = "tcDataGridViewTextBoxColumn";
+            // 
+            // tmDataGridViewTextBoxColumn
+            // 
+            this.tmDataGridViewTextBoxColumn.DataPropertyName = "tm";
+            this.tmDataGridViewTextBoxColumn.HeaderText = "tm";
+            this.tmDataGridViewTextBoxColumn.Name = "tmDataGridViewTextBoxColumn";
+            // 
+            // tpDataGridViewTextBoxColumn
+            // 
+            this.tpDataGridViewTextBoxColumn.DataPropertyName = "tp";
+            this.tpDataGridViewTextBoxColumn.HeaderText = "tp";
+            this.tpDataGridViewTextBoxColumn.Name = "tpDataGridViewTextBoxColumn";
+            // 
+            // t0DataGridViewTextBoxColumn
+            // 
+            this.t0DataGridViewTextBoxColumn.DataPropertyName = "t0";
+            this.t0DataGridViewTextBoxColumn.HeaderText = "t0";
+            this.t0DataGridViewTextBoxColumn.Name = "t0DataGridViewTextBoxColumn";
+            // 
+            // cPMElementBindingSource1
+            // 
+            this.cPMElementBindingSource1.DataSource = typeof(AGH_Logistyka_p1.CPM_Element);
+            // 
+            // idDataGridViewTextBoxColumn1
+            // 
+            this.idDataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn1.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn1.Name = "idDataGridViewTextBoxColumn1";
+            // 
+            // nameDataGridViewTextBoxColumn1
+            // 
+            this.nameDataGridViewTextBoxColumn1.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn1.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn1.Name = "nameDataGridViewTextBoxColumn1";
+            // 
+            // previousElementDataGridViewTextBoxColumn1
+            // 
+            this.previousElementDataGridViewTextBoxColumn1.DataPropertyName = "PreviousElement";
+            this.previousElementDataGridViewTextBoxColumn1.HeaderText = "PreviousElement";
+            this.previousElementDataGridViewTextBoxColumn1.Name = "previousElementDataGridViewTextBoxColumn1";
+            // 
+            // tcDataGridViewTextBoxColumn1
+            // 
+            this.tcDataGridViewTextBoxColumn1.DataPropertyName = "tc";
+            this.tcDataGridViewTextBoxColumn1.HeaderText = "tc";
+            this.tcDataGridViewTextBoxColumn1.Name = "tcDataGridViewTextBoxColumn1";
+            // 
+            // tmDataGridViewTextBoxColumn1
+            // 
+            this.tmDataGridViewTextBoxColumn1.DataPropertyName = "tm";
+            this.tmDataGridViewTextBoxColumn1.HeaderText = "tm";
+            this.tmDataGridViewTextBoxColumn1.Name = "tmDataGridViewTextBoxColumn1";
+            // 
+            // tpDataGridViewTextBoxColumn1
+            // 
+            this.tpDataGridViewTextBoxColumn1.DataPropertyName = "tp";
+            this.tpDataGridViewTextBoxColumn1.HeaderText = "tp";
+            this.tpDataGridViewTextBoxColumn1.Name = "tpDataGridViewTextBoxColumn1";
+            // 
+            // t0DataGridViewTextBoxColumn1
+            // 
+            this.t0DataGridViewTextBoxColumn1.DataPropertyName = "t0";
+            this.t0DataGridViewTextBoxColumn1.HeaderText = "t0";
+            this.t0DataGridViewTextBoxColumn1.Name = "t0DataGridViewTextBoxColumn1";
             // 
             // Form1
             // 
@@ -180,10 +311,12 @@
             this.Text = "Metoda CPM";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cPMElementBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cPMElementBindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -202,6 +335,22 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn previousElementDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tcDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tmDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tpDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn t0DataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource cPMElementBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn previousElementDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tcDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tmDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tpDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn t0DataGridViewTextBoxColumn1;
+        private System.Windows.Forms.BindingSource cPMElementBindingSource1;
     }
 }
 
