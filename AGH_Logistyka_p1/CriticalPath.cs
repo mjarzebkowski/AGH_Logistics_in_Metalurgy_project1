2ba6ef62723ad356951fdc82912d3f812c91486d
Maciej <maciej.jarzebkowski@gmail.com>
2017-12-14 23:41:31 +0100

    Add algoritm project
    Project page:
    https://github.com/elerch/Critical-Path-Extension-Method-for-.NET

--- AGH_Logistyka_p1/CriticalPath.cs
+++ AGH_Logistyka_p1/CriticalPath.cs
@@ -14,44 +14,119 @@ namespace AGH_Logistyka_p1
         public int startTime;
         private int[] possiblePachs;
         public byte view = 0;
+        private CriticalPath nexTCriticalPath;
+        private CriticalPath previousCriticalPath;
+
+        //static CriticalPath getByID(int ID)
+        //{
+
+        //    //return 
+        //}
 
         //DataTable CPM = (DataTable)BindingSource.
 
-        private int[] SetPossiblePatchs(CriticalPath elemCriticalPath)
-        {
-            var ints = from field in elemCriticalPath.Name.Split(',').Where((x) => { int dummy; return Int32.TryParse(x, out dummy); })
-                select Int32.Parse(field);
-            return possiblePachs = ints.ToArray();
-        }
-
-        //public int[] SetStartTime(CriticalPath elemCriticalPath)
+        //private int[] SetPossiblePatchs(CriticalPath elemCriticalPath)
         //{
         //    // ten potworek wyciąga wszystkie numery ścieżek oddzielone przecinkiem, z pola Name. Pomija śmieci
         //    var ints = from field in elemCriticalPath.Name.Split(',').Where((x) => { int dummy; return Int32.TryParse(x, out dummy); })
         //        select Int32.Parse(field);
-        //    return ints.ToArray();
+        //    return possiblePachs = ints.ToArray();
         //}
+
+        public void SetPossiblePatchs(CriticalPath elemCriticalPath)
+        {
+            // ten potworek wyciąga wszystkie numery ścieżek oddzielone przecinkiem, z pola Name. Pomija śmieci
+            var ints = from field in elemCriticalPath.Name.Split(',').Where((x) => { int dummy; return Int32.TryParse(x, out dummy);
+                })
+                select Int32.Parse(field);
+            possiblePachs = ints.ToArray();
+        }
+
+        //public void SetPossiblePatchs(CriticalPath elemCriticalPath)
+        //{
+        //    // ten potworek wyciąga wszystkie numery ścieżek oddzielone przecinkiem, z pola Name. Pomija śmieci
+        //    var ints = from field in elemCriticalPath.Name.Split(',').Where((x) => { int dummy; return Int32.TryParse(x, out dummy);
+        //        })
+        //        select Int32.Parse(field);
+        //    possiblePachs = ints.ToArray();
+        //}
+
+        public int[] SetStartTime(CriticalPath elemCriticalPath)
+        {
+            // ten potworek wyciąga wszystkie numery ścieżek oddzielone przecinkiem, z pola Name. Pomija śmieci
+            var ints = from field in elemCriticalPath.Name.Split(',').Where((x) => { int dummy; return Int32.TryParse(x, out dummy); })
+                       select Int32.Parse(field);
+            return ints.ToArray();
+        }
 
         int CalculateCpmElementStartTime(CriticalPath previousElement)
         {
             return previousElement.t0 + previousElement.startTime;
         }
 
+        private static CriticalPath[] WalkListAhead(CriticalPath[] list)
+        {
+            list[0].Eet = list[0].Est + list[0].t0;
+
+            for (int i = 1; i < na; i++)
+            {
+                foreach (CriticalPath activity in list[i].Predecessors)
+                {
+                    if (list[i].Est < activity.Eet)
+                        list[i].Est = activity.Eet;
+                }
+
+                list[i].Eet = list[i].Est + list[i].t0;
+            }
+
+            return list;
+        }
+
+        private static CriticalPath[] WalkListAback(CriticalPath[] list)
+        {
+            list[na - 1].Let = list[na - 1].Eet;
+            list[na - 1].Lst = list[na - 1].Let - list[na - 1].t0;
+
+            for (int i = na - 2; i >= 0; i--)
+            {
+                foreach (CriticalPath activity in list[i].Successors)
+                {
+                    if (list[i].Let == 0)
+                        list[i].Let = activity.Lst;
+                    else
+                    if (list[i].Let > activity.Lst)
+                        list[i].Let = activity.Lst;
+                }
+
+                list[i].Lst = list[i].Let - list[i].t0;
+            }
+
+            return list;
+        }
 
 
+        List<CPM_Element> _crcPath = new List<CPM_Element>();
+        public static void CalculatePath(List<CriticalPath> projectList)
+        {
+            //projectList.ForEach(item => SetPossiblePatchs(item));
 
-        //List<CPM_Element> _crcPath = new List<CPM_Element>();
-        //public static void CalculatePath(List<CriticalPath> projectList)
-        //{
-        //    foreach ( possiblePachs s in projectList)
-        //    {
+            foreach ( CriticalPath s in projectList )
+            {
+                s.SetPossiblePatchs(s);
+                //s.possiblePachs.Max()
+
+                int start = s.possiblePachs[0];
+                int finish = s.possiblePachs[1];
+
+
                 
-        //    }
-        //    projectList[projectList.Count()].possiblePachs = SetPossiblePatchs();
-                
-                
-        //        .possiblePachs.;
 
+
+                //s.Id;
+
+            }
+            
+            //projectList.Sort(  );
             //weż element o najwyższym oczku
             //w których ścieżkach uczestniczy ?
 
@@ -64,20 +139,32 @@ namespace AGH_Logistyka_p1
         }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
         //https://www.codeproject.com/Articles/25312/Critical-Path-Method-Implementation-in-C
 
         //public class Activity
         //{
-        //    private string id;
+        //    private string id; // Id
         //    private string description;
-        //    private int duration;
-        //    private int est;
+        //    private int duration; // t0
+        //    private int est; 
         //    private int lst;
         //    private int eet;
         //    private int let;
         //    private Activity[] successors;
         //    private Activity[] predecessors;
- 
+
         //        ...
         //}
 
@@ -91,5 +178,5 @@ namespace AGH_Logistyka_p1
         //{
         //    throw new NotImplementedException();
         //}
-    //}
+    }
 }
---
+++ Activity/Activity.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity
+{
+    public class Activity
+    {
+        private string id;
+        private string description;
+        private int duration;
+        private int est;
+        private int lst;
+        private int eet;
+        private int let;
+        private Activity[] successors;
+        private Activity[] predecessors;
+
+        private static Activity[] WalkListAhead(Activity[] list)
+        {
+            list[0].eet = list[0].est + list[0].duration;
+
+            for (int i = 1; i < na; i++)
+            {
+                foreach (Activity activity in list[i].predecessors)
+                {
+                    if (list[i].est < activity.eet)
+                        list[i].est = activity.eet;
+                }
+
+                list[i].eet = list[i].est + list[i].duration;
+            }
+
+            return list;
+        }
+
+        private static Activity[] WalkListAback(Activity[] list)
+        {
+            list[na - 1].let = list[na - 1].eet;
+            list[na - 1].lst = list[na - 1].let - list[na - 1].duration;
+
+            for (int i = na - 2; i >= 0; i--)
+            {
+                foreach (Activity activity in list[i].successors)
+                {
+                    if (list[i].let == 0)
+                        list[i].let = activity.lst;
+                    else
+                    if (list[i].let > activity.lst)
+                        list[i].let = activity.lst;
+                }
+
+                list[i].lst = list[i].let - list[i].duration;
+            }
+
+            return list;
+        }
+
+        void CriticalPath(Activity[] list)
+        {
+            Console.Write("\n          Critical Path: ");
+
+            foreach (Activity activity in list)
+            {
+                if ((activity.eet - activity.let == 0) && (activity.est - activity.lst == 0))
+                    Console.Write("{0} ", activity.id);
+            }
+
+            Console.Write("\n\n         Total duration: {0}\n\n", list[list.Length - 1].eet);
+        }
+    }
+}
